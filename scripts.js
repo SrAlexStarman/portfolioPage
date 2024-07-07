01d3b6466f1a647f26509ef44ed4debee25ebebc
Alejandro Lopez <alejandro.lpz.munoz@gmail.com>
2024-07-07 15:36:47 +0100

    video projects

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
-function switchMain() {
+function switchAboutToProjects() {
     const main1 = document.getElementById('main1');
     const main2 = document.getElementById('main2');
     
     if (main1.classList.contains('active')) {
         main1.classList.remove('active');
-        setTimeout(() => main2.classList.add('active'), 50);
-    } else {
+        main2.classList.add('active'); 
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+};
+
+function switchProjectsToAbout() {
+    const main1 = document.getElementById('main1');
+    const main2 = document.getElementById('main2');
+    
+    if (main2.classList.contains('active')) {
         main2.classList.remove('active');
-        setTimeout(() => main1.classList.add('active'), 50);
+        main1.classList.add('active'); 
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 };
